dc7de6f49187fc63fb98f2fb1912ffc03599ac47
Carrie Christenson <christenson.carrie@gmail.com>
2015-04-09 11:27:46 +0100

    setup login/logout/signup links

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 context "user not signed in and on the homepage" do
   it "should see a 'sign in' link and a 'sign up' link" do
-    visit('/users/signup')
+    visit('/')
     expect(page).to have_link('Sign in')
     expect(page).to have_link('Sign up')
   end
