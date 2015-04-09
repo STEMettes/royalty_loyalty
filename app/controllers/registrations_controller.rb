82499fcd11ee5e99ef8947cd4c4f05d367507080
Rizwan Ali <rizcube@gmail.com>
2015-04-09 14:20:05 +0100

    added registration controller and admin panel

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,13 @@
+class RegistrationsController < Devise::RegistrationsController
+
+    private
+
+  def sign_up_params
+    params.require(:user).permit(:name, :username, :email, :DOB, :county, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:name, :username, :email, :DOB, :county, :password, :password_confirmation)
+  end
+
+end
