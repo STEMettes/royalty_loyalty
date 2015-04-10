e82776a15e3f384839dd8c1062959231f6ba0f9a
Rizwan Ali <rizcube@gmail.com>
2015-04-10 11:29:59 +0100

    user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
+  before_action :authenticate_user!, :except => [:index]
+
+
 
 end
