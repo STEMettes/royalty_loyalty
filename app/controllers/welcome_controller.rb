674bc37ed853f58e3bacdea59215d4355044d032
Carrie Christenson <christenson.carrie@gmail.com>
2015-04-14 12:41:40 +0100

    limited event checkins with logic

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -16,13 +16,33 @@ class WelcomeController < ApplicationController
 
     current_event = Event.find_by(:code => params[:SecretCode])
 
-    if current_event != nil && params[:SecretCode] == Event.find_by(:code => params[:SecretCode]).code then
-      Survey.create(:event_id => current_event.id, :user_id => current_user.id, :survey_type => 'pre-event')
-      redirect_to survey_path
+    if current_user.surveys.last != nil
 
+      last_event_survey = current_user.surveys.last.event.name
+
+      if current_event != nil && params[:SecretCode] == Event.find_by(:code => params[:SecretCode]).code && current_event.name != last_event_survey then
+        Survey.create(:event_id => current_event.id, :user_id => current_user.id, :survey_type => 'pre-event')
+        redirect_to survey_path
+
+      else
+        if current_event.name == last_event_survey
+          flash[:notice] = 'You have already checked-in to this event'
+        else
+          flash[:notice] = 'Incorrect event code please try again' 
+      end
+        redirect_to checkin_path
+      end
+    
     else
-      flash[:notice] = 'Incorrect event code please try again'
-      redirect_to checkin_path
+
+      if current_event != nil && params[:SecretCode] == Event.find_by(:code => params[:SecretCode]).code
+        Survey.create(:event_id => current_event.id, :user_id => current_user.id, :survey_type => 'pre-event')
+        redirect_to survey_path
+
+      else
+        flash[:notice] = 'Incorrect event code please try again' 
+        redirect_to checkin_path
+      end
     end
   end
 
