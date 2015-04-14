58c0a944b50850eea9ad566ce83dbe297a95e0fd
Carrie Christenson <christenson.carrie@gmail.com>
2015-04-14 17:51:47 +0100

    fixed duplicate checkout logic

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -57,31 +57,33 @@ class WelcomeController < ApplicationController
     if current_user.surveys.last != nil
 
       last_event_survey = current_user.surveys.last.event.name
+      post_event = current_user.surveys.last.survey_type
 
-      if current_event != nil && params[:SecretCode] == Event.find_by(:code => params[:SecretCode]).code && current_event.name != last_event_survey then
+      if current_event != nil && params[:SecretCode] == Event.find_by(:code => params[:SecretCode]).code && post_event != 'post-event' then
         Survey.create(:event_id => current_event.id, :user_id => current_user.id, :survey_type => 'post-event')
-        redirect_to survey_path
+        redirect_to post_survey_path
 
       else
-        if current_event.name == last_event_survey
-          flash[:notice] = 'You have already checked-in to this event'
+        if post_event == 'post-event'
+          flash[:notice] = 'You have already checked-out of this event'
         else
           flash[:notice] = 'Incorrect event code please try again' 
       end
-        redirect_to checkin_path
+        redirect_to checkout_path
       end
-    
-    else
 
+    else
       if current_event != nil && params[:SecretCode] == Event.find_by(:code => params[:SecretCode]).code
-        Survey.create(:event_id => current_event.id, :user_id => current_user.id, :survey_type => 'pre-event')
-        redirect_to survey_path
+        Survey.create(:event_id => current_event.id, :user_id => current_user.id, :survey_type => 'post-event')
+        redirect_to post_survey_path
 
       else
         flash[:notice] = 'Incorrect event code please try again' 
-        redirect_to checkin_path
+        redirect_to checkout_path
       end
     end
+
+    
   end
 
 
