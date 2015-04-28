0faeb90c1b35f99e7a3cf785b4a893f14d7e644c
Tom Coakes <tomcoakes@gmail.com>
2015-04-28 12:23:07 +0100

    Migration created for adding all question columns to survey model

---
+++ db/migrate/20150428112052_add_q6to27_columns_to_surveys.rb
@@ -0,0 +1,26 @@
+class AddQ6to27ColumnsToSurveys < ActiveRecord::Migration
+  def change
+    add_column :surveys, :Q6, :string
+    add_column :surveys, :Q7, :string
+    add_column :surveys, :Q8, :string
+    add_column :surveys, :Q9, :string
+    add_column :surveys, :Q10, :string
+    add_column :surveys, :Q11, :string
+    add_column :surveys, :Q12, :string
+    add_column :surveys, :Q13, :string
+    add_column :surveys, :Q14, :string
+    add_column :surveys, :Q15, :string
+    add_column :surveys, :Q16, :string
+    add_column :surveys, :Q17, :string
+    add_column :surveys, :Q18, :string
+    add_column :surveys, :Q19, :string
+    add_column :surveys, :Q20, :string
+    add_column :surveys, :Q21, :string
+    add_column :surveys, :Q22, :string
+    add_column :surveys, :Q23, :string
+    add_column :surveys, :Q24, :string
+    add_column :surveys, :Q25, :string
+    add_column :surveys, :Q26, :string
+    add_column :surveys, :Q27, :string
+  end
+end
